69970619ac2d394191d0301b8dfd4cb6d34f1b87
Toshiki-31159 <t.toshiki.101@users.noreply.github.com>
2020-11-19 17:47:43 +0900

    リビング、カギのサイズと位置設定

--- escape-game/GameScene.swift
+++ escape-game/GameScene.swift
@@ -11,6 +11,20 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
+    //リビングのサイズ、位置設定
+        let floor = SKSpriteNode(imageNamed: "リビング")
+        floor.position = CGPoint(x: 0, y: 20)
+        floor.zPosition = 0
+        floor.size = CGSize(width: 770, height: 450)
+        addChild(floor)
+    //鍵のサイズ、位置設定
+        let key = SKSpriteNode(imageNamed: "カギ")
+        key.position = CGPoint(x: 0, y: -70)
+        key.zPosition = 1
+        key.zRotation = 45
+        key.size = CGSize(width: 25, height: 50)
+        addChild(key)
+
     }
     
     
