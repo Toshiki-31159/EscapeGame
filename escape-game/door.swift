3ba31d6205ce5d39fe691aa43d88e8b9b4316699
Toshiki-31159 <t.toshiki.101@users.noreply.github.com>
2020-11-23 09:50:34 +0900

    戻るボタンの実装

--- escape-game/door.swift
+++ escape-game/door.swift
@@ -10,13 +10,22 @@ import SpriteKit
 import GameplayKit
 
 class Door: SKScene {
+    var backButton = SKSpriteNode()
     
     override func didMove(to view: SKView) {
     //ドアの表示
         let door = SKSpriteNode(imageNamed: "ドア")
+        door.zPosition = 0
         door.size = CGSize(width: 300, height: 400)
         door.name = "door"
         addChild(door)
+    //バックボタンの表示
+        self.backButton = SKSpriteNode(imageNamed: "青矢印バック")
+        self.backButton.zPosition = 1
+        self.backButton.size = CGSize(width: 50, height: 50)
+        self.backButton.position = CGPoint(x: 300, y: -130)
+        self.backButton.name = "backButton"
+        addChild(backButton)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -28,6 +37,13 @@ class Door: SKScene {
             if toucheNode.name == "door" {
                 let scene = Clear(fileNamed: "clearView")
                 let transition = SKTransition.fade(withDuration: 2.0)
+                scene?.scaleMode = SKSceneScaleMode.aspectFill
+                self.view!.presentScene(scene!, transition: transition)
+            }
+        //リビングへ戻る
+            if toucheNode.name == "backButton" {
+                let scene = GameScene(fileNamed: "GameScene")
+                let transition = SKTransition.fade(withDuration: 0.5)
                 scene?.scaleMode = SKSceneScaleMode.aspectFill
                 self.view!.presentScene(scene!, transition: transition)
             }
